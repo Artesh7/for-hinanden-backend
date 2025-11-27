d081ecd87c48bd5621176a27e2641d1c456af625
Artesh7 <333075@via.dk>
2025-11-27 10:51:52 +0100

    ......

--- ForHinanden/ForHinanden.Api/Controllers/FeedbackController.cs
+++ ForHinanden/ForHinanden.Api/Controllers/FeedbackController.cs
@@ -71,7 +71,7 @@ namespace ForHinanden.Api.Controllers
 
             return Ok(feedbacks);
         }
-
+//ss
         // GET: /api/feedback/{deviceId}
         [HttpGet("{deviceId}")]
         public async Task<IActionResult> GetOne(string deviceId)
